2ff914c31532784985548ac2359f43a12716d10c
Connor Stokes <connorjordanstokes@gmail.com>
2024-10-01 10:21:54 +0100

    connor  stokes

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,10 +1,45 @@
-function ArtsSection() {
+import React, { useState, useEffect } from "react";
+
+const Art = () => {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    const fetchArtworks = async () => {
+      const response = await fetch("https://boolean-uk-api-server.fly.dev/art");
+      const data = await response.json();
+      setArtworks(data);
+    };
+
+    fetchArtworks();
+  }, []);
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className="art-list">
+          {artworks.map((art) => (
+            <li key={art.id}>
+              <div className="frame">
+                <img
+                  src={`https://boolean-uk-api-server.fly.dev${art.imageURL}`}
+                  alt={art.title}
+                />
+              </div>
+              <h3>{art.title}</h3>
+              <p>Artist: {art.artist}</p>
+              <h4>Publication History:</h4>
+              <ul>
+                {art.publicationHistory.map((history, index) => (
+                  <li key={index}>{history}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ArtsSection
+export default Art;
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,10 +1,37 @@
-function UsersSection() {
+import React, { useState, useEffect } from "react";
+import md5 from "md5";
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("https://boolean-uk-api-server.fly.dev/ConnorJS21/contact");
+      const data = await response.json();
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <section>
-      <h2>Users Section</h2>
-      <div className="scroll-container"></div>
-    </section>
-  )
-}
+    <h2>Users Section</h2>
+    <div className="scroll-container">
+      <ul className="users-list">
+        {users.map((user) => (
+          <li key={user.id} style={{ background: user.favouriteColour }}>
+            <img
+              src={`https://www.gravatar.com/avatar/${md5(user.email.trim().toLowerCase())}?s=120&d=identicon`}
+              alt={`${user.firstName} ${user.lastName}`}
+            />
+            <h3>{user.firstName} {user.lastName}</h3>
+            <p>Email: {user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </section>
+  );
+};
 
-export default UsersSection
+export default Users;
